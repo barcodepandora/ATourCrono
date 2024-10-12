b8e2e5196ce0c4b6cd3a547573ab4ed8ddc7fdc7
Juan <jmoreno@novopayment.com>
2024-10-12 14:01:02 -0500

    Stub show detail

--- ATourCrono/ATourCrono/ContentView.swift
+++ ATourCrono/ATourCrono/ContentView.swift
@@ -9,21 +9,70 @@ import SwiftUI
 import MapKit
 
 struct ContentView: View {
+    
+    @State private var region = MKCoordinateRegion(
+        center: CLLocationCoordinate2D(latitude: 23.5665976, longitude: 58.3843438),
+        span: MKCoordinateSpan(latitudeDelta: 0.005, longitudeDelta: 0.005)
+    )
+    @State private var showDetails = false
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
         }
         .padding()
         ClockView()
-        Map(coordinateRegion: .constant(MKCoordinateRegion(
-            center: CLLocationCoordinate2D(
-                latitude: 23.5665976, longitude: 58.3843438),
-            span: MKCoordinateSpan(
-                latitudeDelta: 0.005, longitudeDelta: 0.005)))
+        Map(coordinateRegion: $region, annotationItems: [Mark(name: "Marker", coordinate: CLLocationCoordinate2D(latitude: 23.5665976, longitude: 58.3843438))]) { mark in
+            MapAnnotation(coordinate: mark.coordinate) {
+                Image(systemName: "mappin.circle.fill")
+                    .font(.largeTitle)
+                    .foregroundColor(.red)
+                    .onTapGesture {
+                        showDetails = true
+                    }
+            }
+        }
+        .overlay(
+            GeometryReader { geometry in
+                ZStack {
+                    if self.showDetails {
+                        DetailView(showDetails: $showDetails, name: "Marker Details")
+                            .frame(width: 242, height: 500)
+                            .background(Color.white)
+                            .cornerRadius(10)
+                            .shadow(radius: 10)
+                            .offset(x: geometry.size.width - 255, y: geometry.size.height - 661)
+                    }
+                }
+            }
         )
+    }
+}
+
+struct Mark: Identifiable {
+    let id = UUID()
+    let name: String
+    let coordinate: CLLocationCoordinate2D
+}
+
+struct DetailView: View {
+    @Binding var showDetails: Bool
+    
+    @Environment(\.dismiss) private var dismiss
+    
+    let name: String
+
+    var body: some View {
+        VStack {
+            Text(name)
+                .foregroundColor(.black)
+                .font(.largeTitle)
+            Text("This is a detail view")
+                .font(.body)
+            Button("Dismiss") {
+                showDetails.toggle()
+            }
+        }
+        .padding()
     }
 }
 
